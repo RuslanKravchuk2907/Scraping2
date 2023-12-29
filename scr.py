b2bf3ee7870fe177a3a1c73865638973ee7fe23e
melad <meladzeruslan164@gmail.com>
2023-12-29 22:27:04 +0200

    29.12

---
+++ scr.py
@@ -0,0 +1,21 @@
+from requests import Session
+from bs4 import BeautifulSoup
+from time import sleep
+
+headers = {"User-Agent": "Mozilla/5.0, AppleWebKit/537.36, Chrome/120.0.0.0"}
+
+work = Session()
+
+work.get("https://quotes.toscrape.com/", headers=headers)
+
+response = work.get("https://quotes.toscrape.com/login", headers=headers)
+
+soup = BeautifulSoup(response.text, "lxml")
+
+token = soup.find("form").find("input").get("value")
+
+data = {"csrf_token": token, "username": "noname", "password": "password"}
+
+result = work.post("https://quotes.toscrape.com/login", headers=headers, data=data, allow_redirects=True)
+
+print(result.text)
